0260e6b902bec7d71ba1352de5ec2b144968c9c1
jajalla23 <jajalla23@gmail.com>
2014-05-30 20:14:04 -0400

    rules-plan event handlers

---
+++ interface/super/rules/library/RulesPlanMappingEventHandlers.php
@@ -0,0 +1,43 @@
+<?php
+class RuleTransaction {
+	public $ruleid;
+	public $transactionType;
+}
+
+function getPlans() {
+	//TODO: Implement code to return list of plans
+	 
+	return null;
+}
+
+function getRulesOfPlan($listOfPlans) {
+	//TODO: implement code to return rules for each plan in the input
+	
+	return null;
+}
+
+function getRulesNotInPlan($listOfPlans) {
+	//TODO: implement code to return rules that are not part of each plan in the input
+	
+	return null;
+}
+
+function addNewPlan($plan_name, $plan_rules) {
+	//TODO: implement code to add a new plan with its corresponding rules
+	
+	return null;
+}
+
+function deletePlan($plan_id) {
+	
+}
+
+function togglePlanStatus($plan_id, $isActive) {
+	
+}
+
+function submitChanges($plan_id, $RuleTransactionList) {
+	
+}
+
+?>
