72bf585db56b056948d75d29a7a5672d9710efc3
jajalla23 <jajalla23@gmail.com>
2014-06-28 19:20:19 -0400

    cdr change

--- interface/super/rules/library/RulesPlanMappingEventHandlers.php
+++ interface/super/rules/library/RulesPlanMappingEventHandlers.php
@@ -86,6 +86,9 @@ if ($_GET["action"] == "getNonCQMPlans") {
 	} else if (strlen($plan_id) > 0) {
 		submitChanges($plan_id, $added_rules, $removed_rules);
 	}	
+	
+} else if ($_GET["action"] == "deletePlan") {
+	deletePlan($_GET["plan_id"]);
 }
 
 
@@ -128,7 +131,7 @@ function getRulesNotInPlan($plan_id) {
 	$sql_st = "SELECT lst_opt.option_id as rule_option_id, lst_opt.title as rule_title " .
 				"FROM `clinical_rules` clin_rules " .
 				"JOIN `list_options` lst_opt ON lst_opt.option_id = clin_rules.id " .
-				"WHERE lst_opt.option_id NOT IN " .
+				"WHERE clin_rules.cqm_flag = 0 AND lst_opt.option_id NOT IN " .
 					"( " .
 					"SELECT lst_opt.option_id " .
 					"FROM `clinical_plans_rules` cpr " .
@@ -145,23 +148,47 @@ function getRulesNotInPlan($plan_id) {
 }
 
 function addNewPlan($plan_name, $plan_rules) {
-	$plan_id = strtolower(preg_replace('/\s+/', '_', $plan_name)) . '_plan';
+	//$plan_id = strtolower(preg_replace('/\s+/', '_', $plan_name)) . '_plan';
 	
-	/** clinical_plans **/
+	/** plan_id start **/
+	$plan_id = 1;
+	$sql_st = "SELECT MAX(SUBSTR(clin_plans.id, 1, LOCATE('_plan', clin_plans.id)-1)) as max_planid " .
+				"FROM `clinical_plans` clin_plans " .
+				"WHERE clin_plans.id like '%_plan' AND SUBSTR(clin_plans.id, 1, LOCATE('_plan', clin_plans.id)) REGEXP '[0-9]+'; ";
+	$res = sqlStatement($sql_st, null);
+	
+	if ($res != NULL) {
+		while($row = sqlFetchArray($res)) {
+			$plan_id = $row['max_planid'];
+		}
+		$plan_id += 1;
+	}
+	
+	$plan_id = $plan_id . '_plan';
+	
+	/** plan_id end **/
+	
+	
+	/** clinical_plans start **/
 	$sql_st = "INSERT INTO `clinical_plans` (`id`, `pid`, `normal_flag`, `cqm_flag`, `cqm_measure_group`) " . 
 				"VALUES (?, 0, 1, 0, '');";
 	$res = sqlStatement($sql_st, array($plan_id));
 	
+	/** clinical_plans end **/
 	
-	/** list_options **/
+	
+	/** list_options start **/
 	$sql_st = "SELECT MAX(`seq`) AS max_seq " .
 				"FROM `list_options` " .
-				"WHERE `list_id` = 'clinical_plans' " .
-				"ORDER BY `list_options`.`seq`;";
+				"WHERE `list_id` = 'clinical_plans'; ";
 	$res = sqlStatement($sql_st, null);
 	$max_seq = 0;
-	while($row = sqlFetchArray($res)) {
-		$max_seq = $row['max_seq'] + 10;
+	
+	if ($res != NULL) {
+		while($row = sqlFetchArray($res)) {
+			$max_seq = $row['max_seq'];
+		}
+		$max_seq += 10;
 	}
 	
 	$sql_st = "INSERT INTO `list_options` " .
@@ -169,7 +196,10 @@ function addNewPlan($plan_name, $plan_rules) {
 				"VALUES ('clinical_plans', ?, ?, ?, 0, 0, '', '', '');";
 	$res = sqlStatement($sql_st, array($plan_id, $plan_name, $max_seq));
 	
-	/** rules **/
+	/** list_options end **/
+	
+	
+	/** rules start **/
 	addRulesToPlan($plan_id, $plan_rules);
 	
 	/*
@@ -184,8 +214,14 @@ function addNewPlan($plan_name, $plan_rules) {
 }
 
 function deletePlan($plan_id) {
-	//TODO:
+	$sql_st = "DELETE FROM `clinical_plans` WHERE `clinical_plans`.`id` = ?;";
+	$res = sqlStatement($sql_st, array($plan_id));
 	
+	$sql_st = "DELETE FROM `list_options` WHERE `list_id` = 'clinical_plans' AND `option_id` = ?;";
+	$res = sqlStatement($sql_st, array($plan_id));
+	
+	$sql_st = "DELETE FROM `clinical_plans_rules` WHERE `plan_id` = ?;";
+	$res = sqlStatement($sql_st, array($plan_id));
 }
 
 function togglePlanStatus($plan_id, $isActive) {
