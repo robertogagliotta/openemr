f6f40d0c31964ab67a7f20c99750868eda1d880a
roberto <robertogagliotta@gmail.com>
2017-01-25 12:31:01 -0800

    SNOMED RF2 support, take 2.

--- interface/code_systems/standard_tables_manage.php
+++ interface/code_systems/standard_tables_manage.php
@@ -21,6 +21,7 @@
  * @author  (Mac) Kevin McAloon <mcaloon@patienthealthcareanalytics.com>
  * @author  Rohit Kumar <pandit.rohit@netsity.com>
  * @author  Brady Miller <brady.g.miller@gmail.com>
+ * @author  Roberto Vasquez <robertogagliotta@gmail.com>
  * @link    http://www.open-emr.org
  */
 
@@ -77,17 +78,33 @@ if ($db == 'RXNORM') {
     }
 } else if ( $db == 'SNOMED') {
     if ($version == "US Extension") {
-        if (!snomed_import(TRUE)) {
+        //if (!snomed_import(TRUE)) {
+        if (!snomed_import()) {
             echo htmlspecialchars( xl('ERROR: Unable to load the file into the database.'), ENT_NOQUOTES)."<br>";
             temp_dir_cleanup($db);
             exit;
+         } else {
+             drop_old_sct2();
          }
+    }
+    if (strncmp($version, "RF2", 3) == 0 ) {
+        $version = ltrim($version, "RF2");  
+        //if (!snomedRF2_import(FALSE)) {
+        if (!snomedRF2_import()) {
+            echo htmlspecialchars( xl('ERROR: Unable to load the file into the database.'), ENT_NOQUOTES)."<br>";
+            temp_dir_cleanup($db);
+            exit;
+        } else {
+            drop_old_sct();
+        }
     }
     else { //$version is not "US Extension"
         if (!snomed_import(FALSE)) {
             echo htmlspecialchars( xl('ERROR: Unable to load the file into the database.'), ENT_NOQUOTES)."<br>";
             temp_dir_cleanup($db);
             exit;
+         } else {
+             drop_old_sct2();
          }
     }
 }
